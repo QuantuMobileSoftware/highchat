ff5e2f10450c09e9346e15cbc0e2b7ad74c7c0b9
Dmitry Yakutkin <dmitry.yakutkin@gmail.com>
2016-10-21 18:38:33 +0300

    Initial test implementation

--- highchat/tests.py
+++ highchat/tests.py
@@ -2,26 +2,29 @@ import os
 import tornado.ioloop
 import tornado.httpclient as client
 
-from unittest import TestCase
+from tornado.testing import AsyncTestCase, gen_test, main
 
 
 URLS = [
-    'https://www.google.com.ua',
-    'http://www.tornadoweb.org/',
-    'https://www.python.org/'
+    'http://info.cern.ch/hypertext/WWW/TheProject.html',
 ]
 
+RESP = '{"links": [{"url": "http://info.cern.ch/hypertext/WWW/TheProject.html", "title": "The World Wide Web project"}]}'
 
-class TestHighChat(TestCase):
+
+class TestHighChat(AsyncTestCase):
     def setUp(self):
+        super().setUp()
         self.app_address = 'http://localhost:{}'.format(os.environ.get('APPLICATION_PORT', '9000'))
         self.request_payload = ' '.join(URLS)
 
-        tornado.ioloop.IOLoop.current().start()
-        self.client = client.AsyncHTTPClient()
-
+    @gen_test(timeout=1000)
     def test_1k_requests(self):
-        request_url = os.path.join(self.app_address, '/analyze')
-        response_futures = [self.client.fetch(request_url) for _ in range(1000)]
+        self.client = client.AsyncHTTPClient(self.io_loop)
+
+        request = client.HTTPRequest(self.app_address + '/analyze', method='POST', body=self.request_payload)
+        response_futures = [self.client.fetch(request) for _ in range(1000)]
         responses = yield response_futures
 
+        for response in responses:
+            self.assertEqual(response.body.decode(), RESP)
